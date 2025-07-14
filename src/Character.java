4b6d23ad1c0bd85dd6f4bc07718c5827ff4c4698
241040 <241040@std.hi-joho.ac.jp>
2025-07-14 12:31:28 +0900

    7/14

--- src/Character.java
+++ src/Character.java
@@ -1,2 +1,16 @@
-public class Character {
+public abstract class Character implements Creature{
+    private String name;
+    private int hp;
+
+    public Character(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+    final public boolean isAlive(){
+        return hp > 0;
+    }
+    public void showStats() {
+        System.out.println(this.name + " : HP " + this.hp);
+    }
+
 }
--- src/Creature.java
+++ src/Creature.java
@@ -1,2 +1,13 @@
-public class Creature {
+public interface Creature {
+    boolean isAlive();
+
+    void showStats();
+
+    void attack(Creature target);
+
+    String getName();
+
+    int getHp();
+
+    void setHp(int hp);
 }
