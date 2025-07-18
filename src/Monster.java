b7c67ea138fbecf2adf5ee6bf053415004988757
241040 <241040@std.hi-joho.ac.jp>
2025-07-18 12:30:38 +0900

    7/16

--- src/Monster.java
+++ src/Monster.java
@@ -14,5 +14,22 @@ public abstract class Monster implements Creature {
     public final boolean isAlive() {
         return hp > 0;
     }
-
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+    public char getSuffix() {
+        return suffix;
+    }
+    public void setSuffix(char suffix) {
+        this.suffix = suffix;
+    }
 }
